5efa357e58e0ce924ce2bb7c3c1014ab8d7c4fa9
ian.moss <ianmsieg@gmail.com>
2022-05-06 16:48:53 -0500

    finished task retrieval function from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -5,26 +5,30 @@ $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
 $('button').on('click', saveTask)
 
-let test = localStorage.getItem('9')
+let test = localStorage.getItem('9');
 
-function popTask() {
-    for (var i = 0; i < hourBlock.length; i++) {
-
-    }
+function getTask() {
+    $('.time-block').each(function() {
+        let time = $(this).children('input').attr('data-time');
+        let desc = localStorage.getItem(time);
+        let text = $(this).children('input');
+        text.val(desc);
+    })
 }
 
 function saveTask() {
     let icon = $(this).children('i')
     icon.removeClass('fa-solid').addClass('fa-regular');
-    setTimeout(function(){
+    setTimeout(function() {
         icon.removeClass('fa-regular').addClass('fa-solid');
-    },350);
+    }, 400);
     let key = $(this).attr('id')
     let task = $(this).parent().children('input').val()
     localStorage.setItem(key, task);
 }
 
 function timeKeep() {
+    getTask();
     for (var i = 0; i < hourBlock.length; i++) {
         if (now < parseInt($(hourBlock[i]).attr('data-time'))) {
             $(hourBlock[i]).addClass('future')
